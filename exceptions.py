9d09d6181941da2cbf4f2cc84fbf7f5d0ca24075
nm0i <nm0i@me0w.net>
2022-12-02 22:51:11 +0300

    .

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,13 @@ class MalformedPracticumReply(Exception):
 
 class UnreachablePracticumEndpoint(Exception):
     pass
+
+
+class MessageSendingFailed(Exception):
+    pass
+
+class PracticumRequestError(Exception):
+    pass
+
+class HomeworkListEmpty(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -27,54 +27,76 @@ HOMEWORK_VERDICTS: Dict[str, str] = {
 }
 
 
+class TelegramFilter(logging.Filter):
+    """Класс фильтра для отправки ошибок в телеграм."""
+
+    last_message: str = ""
+
+    def __init__(self, bot):
+        """Инициализация фильтра для логов."""
+        self.bot = bot
+
+    def filter(self, record):
+        """Правило фильтра."""
+        if (record.levelno == logging.ERROR
+                and self.last_message != record.msg):
+            send_message(self.bot, record.msg)
+            self.last_message = record.msg
+        return True
+
+
 def check_tokens() -> bool:
     """Проверка наличия обязательных переменных окружения."""
-    flag: bool = True
-    env_list: Tuple[str] = ("PRACTICUM_TOKEN",
-                            "TELEGRAM_TOKEN",
-                            "TELEGRAM_CHAT_ID")
-
-    for env_var in env_list:
-        if not os.getenv(env_var):
-            log_message: str = f"Переменная окружения {env_var} не объявлена."
-            logging.critical(log_message)
-            flag = False
-
-    return flag
+    env_list: Tuple[str] = (
+        "PRACTICUM_TOKEN",
+        "TELEGRAM_TOKEN",
+        "TELEGRAM_CHAT_ID"
+    )
+    return all(map(lambda x: os.getenv(x), env_list))
 
 
 def send_message(bot, message: str) -> None:
     """Отправка сообщения ботом телеграмма."""
+    logging.debug(f"Отправляем сообщение в телеграм: {message}.")
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
     except telegram.error.TelegramError:
         logging.error(f"Бот не смог отправить сообщение {message}",
                       {"tg_error": True})
     else:
-        logging.debug(f"Бот отправил сообщение: {message}",
-                      {"tg_error": True})
+        logging.debug(f"Бот отправил сообщение: {message}")
 
 
 def get_api_answer(timestamp: int) -> Dict:
     """Получить ответ от практикума."""
-    headers: Dict[str, str] = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
-    payload: Dict[str, str] = {"from_date": str(timestamp)}
-    logging.debug(f"Запрашиваем домашки за {timestamp}")
+    requests_params = {
+        "headers": {"Authorization": f"OAuth {PRACTICUM_TOKEN}"},
+        "params": {"from_date": str(timestamp)},
+    }
+
+    logging.debug(f"Запрашиваем домашки за {timestamp}.")
     try:
-        homework_statuses = requests.get(ENDPOINT,
-                                         headers=headers,
-                                         params=payload)
-    except requests.RequestException as error:
-        log_message = f"Ошибка при запросе к практикуму: {error}"
-        logging.error(log_message)
+        homework_statuses = requests.get(ENDPOINT, **requests_params)
+    except requests.RequestException:
+        raise exceptions.PracticumRequestError(
+            "Ошибка при запросе к практикуму."
+        )
+    except Exception:
+        raise exceptions.PracticumRequestError(
+            "Ошибка при запросе к практикуму"
+        )
 
     if homework_statuses.status_code != 200:
-        log_message: str = (f"Эндпоинт практикума не доступен,"
-                            f" код запроса: {homework_statuses.status_code}")
+        log_message: str = (
+            f"Эндпоинт практикума не доступен, "
+            f"код запроса: {homework_statuses.status_code}, "
+            f"заголовки: {homework_statuses.headers}, "
+            f"тело ответа: {homework_statuses.text}"
+        )
         logging.error(log_message)
         raise exceptions.UnreachablePracticumEndpoint
     else:
-        log_message: str = "Ответ от практикума получен"
+        log_message: str = "Ответ от практикума получен."
         logging.debug(log_message)
 
     return homework_statuses.json()
@@ -82,10 +104,17 @@ def get_api_answer(timestamp: int) -> Dict:
 
 def check_response(response) -> None:
     """Проверка наличия домашних работ в ответе от практикума."""
-    if type(response) is not dict:
+    if not isinstance(response, Dict):
         raise TypeError
-    if type(response.get("homeworks")) is not list:
+    if not isinstance(response.get("homeworks"), List):
         raise TypeError
+    if "code" in response:
+        if response.get("code") == "not_authenticated":
+            raise exceptions.PracticumRequestError("Ошибра авторизации.")
+        if response.get("code") == "UnknownError":
+            raise exceptions.PracticumRequestError("Неизвестная ошибка.")
+    if not len(response.get("homeworks")):
+        raise exceptions.HomeworkListEmpty("Список домашних работ пуст.")
 
 
 def parse_status(homework) -> str:
@@ -106,62 +135,46 @@ def parse_status(homework) -> str:
 
 def main():
     """Основная логика работы бота."""
-
-    class TelegramFilter(logging.Filter):
-        """Класс фильтра для отправки ошибок в телеграм."""
-
-        last_message: str = ""
-
-        def filter(self, record):
-            if ("tg_error" not in record.args
-                    and record.levelno == logging.ERROR
-                    and self.last_message != record.msg):
-                send_message(bot, record.msg)
-                self.last_message = record.msg
-            return True
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    logger_handler = logging.StreamHandler(sys.stdout)
-    logger_handler.setLevel(logging.DEBUG)
-    log_format: str = "%(asctime)s [%(levelname)s] %(message)s"
-    formatter = logging.Formatter(log_format)
-    logger_handler.setFormatter(formatter)
-
-    logger.addHandler(logger_handler)
-    logger.addFilter(TelegramFilter())
-
     if not check_tokens():
-        logging.critical("Программа принудительно остановлена.")
+        logging.critical(
+            "Не объявлены необходимые переменные окружения. "
+            "Программа принудительно остановлена."
+        )
         os._exit(0)
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    logger.addFilter(TelegramFilter(bot))
 
-    mainloop_period: int = 0
+    last_successful_check: int = int(time.time()) - RETRY_PERIOD
     logging.debug("Входим в главный цыкл.")
     while True:
-        timestamp: int = int(time.time())
-        last_timestamp: int = timestamp - RETRY_PERIOD - mainloop_period
-
         try:
-            practicum_response: Dict = get_api_answer(last_timestamp)
+            practicum_response: Dict = get_api_answer(last_successful_check)
             check_response(practicum_response)
             homeworks: List = practicum_response.get("homeworks")
-            if len(homeworks):
-                for homework in homeworks:
-                    log_message: str = parse_status(homework)
-                    send_message(bot, log_message)
-            else:
-                log_message: str = "Изменений в статусах домашек нет."
-                logger.debug(log_message)
+            for homework in homeworks:
+                log_message: str = parse_status(homework)
+                send_message(bot, log_message)
         except Exception as error:
-            log_message = f"Сбой в работе программы: {error}"
+            log_message = f"Сбой в работе программы: {str(error)}"
             logging.error(log_message)
-
-        mainloop_period = int(time.time()) - timestamp
+        else:
+            last_successful_check = int(time.time())
 
         time.sleep(RETRY_PERIOD)
 
 
 if __name__ == "__main__":
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger_handler = logging.StreamHandler(sys.stdout)
+    logger_handler.setLevel(logging.DEBUG)
+
+    log_format: str = "%(asctime)s [%(levelname)s] %(message)s"
+    formatter = logging.Formatter(log_format)
+    logger_handler.setFormatter(formatter)
+
+    logger.addHandler(logger_handler)
+
     main()
