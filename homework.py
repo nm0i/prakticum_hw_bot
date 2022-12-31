fa779c779db4d541dde72978fa12cf6514746283
nm0i <nm0i@me0w.net>
2022-12-31 15:54:48 +0300

    Unused variable

--- homework.py
+++ homework.py
@@ -16,7 +16,6 @@ PRACTICUM_TOKEN: str = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID: str = os.getenv("TELEGRAM_CHAT_ID")
 
-RETRY_PERIOD: int = 600
 ENDPOINT: str = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS: dict[str, str] = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
